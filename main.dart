d19e6e54e211703b14a4470e95b12888d99e68ad
Br28 <brunosimachulu28@gmail.com>
2024-03-28 15:12:35 +0200

    dart datatypes assignment done

---
+++ main.dart
@@ -0,0 +1,33 @@
+void main(List<String> args) {
+  // Datatypes assignment
+
+  // int
+  int number1 = 1000;
+  int number2 = 500;
+  int res = number1 * number2;
+  print(res);
+
+  // double
+  double savings = 1520;
+  double durationInMonths = 5;
+  double totalSavings = savings * durationInMonths;
+  print(totalSavings);
+
+  // String
+  String phrase =
+      'Always practice programming, in order for you to be a great programmer';
+  print(phrase);
+
+  // List
+  List phoneBrands = ['Google Pixel', 'Huawei', 'Samsungs', 'Apple', 'Tecno'];
+  print(phoneBrands);
+
+  // Map
+  Map tvChannels = {
+    'Zambezi Magic': 162,
+    'Supersport 3': 223,
+    'WWE': 128,
+    'TMC': 137
+  };
+  print(tvChannels);
+}
